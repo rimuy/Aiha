c08b57637f6a468bdfb1269798f5d6a6e3d4dc97
NescauBall <kevinntavares@gmail.com>
2020-09-13 00:01:18 -0300

    removing roles that requires a higher level

--- src/Aiha/lib/LevelRoles.js
+++ src/Aiha/lib/LevelRoles.js
@@ -1,15 +1,22 @@
 const { Server } = require('..');
 
+var fetched;
+
 module.exports = async (Bot, user, msg, level) => {
     const roles = await Server.Database.request('GET', 'levelroles');
     const guild = msg.guild;
     const member = await guild.members.fetch(user);
 
+    if (!fetched) {
+        await guild.roles.fetch();
+        fetched = true;
+    }
+
     if (member) {
         const levelRoles = [];
 
-        await Promise.all(Object.keys(roles).map(async roleId => {
-            const role = await guild.roles.fetch(roleId);
+        Object.keys(roles).forEach(roleId => {
+            const role = guild.roles.cache.get(roleId);
             const info = roles[roleId];
             
             if (role && level >= info.requiredLevel) {
@@ -20,10 +27,12 @@ module.exports = async (Bot, user, msg, level) => {
                 });
             
             }
-        }));
+            else if (role && level < info.requiredLevel) 
+                member.roles.remove(role.id).catch();
+        });
 
         const highestLevel = Math.max(...levelRoles.map(r => r.level));
-       
+
         await Promise.all(
             levelRoles
                 .filter(r => r.role.editable)
