a35a235b3606173a5680c00cb0e3e13a5fb8f569
NescauBall <kevinntavares@gmail.com>
2020-09-13 23:36:02 -0300

    Changed main file directory

--- src/Aiha/bot.js
+++ src/Aiha/bot.js
@@ -1,8 +1,8 @@
 console.log('\x1b[35m');
 require('dotenv').config();
 require('moment-timezone').tz.setDefault('America/Sao_Paulo');
-require('./server');
-const { Bot } = require('./src/Aiha');
+require('../../server');
+const { Bot } = require('.');
 
 const bot = new Bot();
 bot.client.login(
