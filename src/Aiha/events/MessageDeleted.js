f02999be46da06530083580bcf2f1716f56991ff
NescauBall <kevinntavares@gmail.com>
2020-09-18 08:11:59 -0300

    Revert "Don"
    This reverts commit 797a98092a651f3cb5c6e76092e280d4aa604788.

--- src/Aiha/events/MessageDeleted.js
+++ src/Aiha/events/MessageDeleted.js
@@ -3,10 +3,7 @@
  */
 
 const { Event, BaseEmbed } = require('..');
-const { Util } = require('discord.js');
 const Logs = require('../lib/Logs');
-
-const userMessages = new Map();
 
 class MessageDeleteEvent extends Event {
     constructor() {
@@ -16,20 +13,10 @@ class MessageDeleteEvent extends Event {
 
                 if (!msg.content) return;
 
-                const user = msg.author;
+                const logs = await msg.guild.fetchAuditLogs({ type: 72 });
+                const entry = logs.entries.first();
 
-                !userMessages.has(user.id)
-                    && userMessages.set(user.id, new Set());
-
-                const collected = userMessages.get(user.id);
-                collected.add(msg.id);
-
-                await Util.delayFor(900);
-
-                const size = collected.size;
-                collected.clear();
-
-                if (size > 1) return;
+                if (entry.reason === 'spam') return;
                 
                 const embed = new BaseEmbed()
                     .setAuthor('Mensagem Deletada', msg.author.displayAvatarURL({ dynamic: true }))
