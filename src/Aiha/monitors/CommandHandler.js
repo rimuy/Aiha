a62396048eeb3b561646e52017df5e73448db93c
NescauBall <kevinntavares@gmail.com>
2020-09-22 12:02:46 -0300

    Classes

--- src/Aiha/monitors/CommandHandler.js
+++ src/Aiha/monitors/CommandHandler.js
@@ -1,16 +1,19 @@
 
-module.exports = Bot => {
-    const cmds = require('../config/js/Commands');
+class CommandHandler {
+    constructor(Bot) {
+        const cmds = require('../config/js/Commands');
 
-    cmds.forEach(cmdClass => {
-        const cmd = new cmdClass();
+        cmds.forEach(cmdClass => {
+            const cmd = new cmdClass();
 
-        Bot.commands.set(cmd.name.toLowerCase(), cmd);
+            Bot.commands.set(cmd.name.toLowerCase(), cmd);
 
-        cmd.aliases.forEach(alias => {
-            Bot.aliases.set(alias.toLowerCase(), cmd);
+            cmd.aliases.forEach(alias => {
+                Bot.aliases.set(alias.toLowerCase(), cmd);
+            });
         });
-    });
 
-    return cmds;
-};
+    }
+}
+
+module.exports = CommandHandler;
--- src/Aiha/monitors/EventListener.js
+++ src/Aiha/monitors/EventListener.js
@@ -4,21 +4,24 @@ const Log = require('../util/Log');
 
 const eventsDir = readdirSync(path);
 
-module.exports = Bot => {
-    const files = eventsDir.filter(f => f.endsWith('.js'));
+class EventListener {
+    constructor(Bot) {
+        const files = eventsDir.filter(f => f.endsWith('.js'));
 
-    files.forEach(f => {
-        
-        try {
-            const exports = require(path + f);
-            const event = new exports();
+        files.forEach(f => {
+            
+            try {
+                const exports = require(path + f);
+                const event = new exports();
 
-            Bot.client.on(event.name, (...args) => event.run(Bot, ...args));
-            Bot.events.set(event.name, event);
-        } catch(e) {
-            Log('FG_RED', `[${f}] ` + e.message);
-        }
-        
-    });
+                Bot.client.on(event.name, (...args) => event.run(Bot, ...args));
+                Bot.events.set(event.name, event);
+            } catch(e) {
+                Log('FG_RED', `[${f}] ` + e.message);
+            }
+            
+        });
+    }
+}
 
-};
+module.exports = EventListener;
