a5889171ff2a4e95208ca36f9786c2fa7c3142d2
NescauBall <kevinntavares@gmail.com>
2020-09-22 19:39:40 -0300

    Member counter fix

--- src/Aiha/monitors/GuildStatus.js
+++ src/Aiha/monitors/GuildStatus.js
@@ -22,22 +22,22 @@ class GuildStatus {
         }
 
         const emojis = {
-            '0': ':0_:',
-            '1': ':1_:',
-            '2': ':2_:',
-            '3': ':3_:',
-            '4': ':4_:',
-            '5': ':5_:',
-            '6': ':6_:',
-            '7': ':7_:',
-            '8': ':8_:',
-            '9': ':9_:',
+            '0': guild.emojis.cache.get('0_'),
+            '1': guild.emojis.cache.get('1_'),
+            '2': guild.emojis.cache.get('2_'),
+            '3': guild.emojis.cache.get('3_'),
+            '4': guild.emojis.cache.get('4_'),
+            '5': guild.emojis.cache.get('5_'),
+            '6': guild.emojis.cache.get('6_'),
+            '7': guild.emojis.cache.get('7_'),
+            '8': guild.emojis.cache.get('8_'),
+            '9': guild.emojis.cache.get('9_'),
         };
 
         membersSize = (' ' + ZeroWidthSpace).repeat(64) + '00000'.slice(0, -(membersSize.toString()).length) + membersSize.toString();
 
         const topics = [
-            '`Membros` ' + membersSize.split('').map(n => emojis[n] || '').join(''),
+            'Membros: ' + membersSize.split('').map(n => emojis[n] || '').join(''),
         ];
 
         channel.setTopic(`${topics.join('\n')}`);
