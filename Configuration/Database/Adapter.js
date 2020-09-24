a966316b5c79bdbe4b74e302be5b506a79db2cd6
NescauBall <kevinntavares@gmail.com>
2020-09-24 17:57:27 -0300

    Adapter fix

--- Configuration/Database/Adapter.js
+++ Configuration/Database/Adapter.js
@@ -1,5 +1,5 @@
 const FileSync = require('lowdb/adapters/FileSync');
 const Path = require('path');
-const adapter = new FileSync(Path.resolve(__dirname, '..', 'Adapter.json'));
+const adapter = new FileSync(Path.resolve(__dirname, '..', '..', 'Server', 'Adapter.json'));
 
 module.exports = adapter;
