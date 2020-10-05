8d936ea2a2797275114a7a30d5b01a108e50565f
Kevin <kevinntavares@gmail.com>
2020-10-05 07:04:24 -0300

    Added channel notification to auto-backup

--- Monitors/BackupManager.js
+++ Monitors/BackupManager.js
@@ -31,7 +31,16 @@ class BackupManager {
             settings.lastBackup = Date.now();
             lastUpdate = settings.lastBackup;
 
-            await Server.Database.request('PATCH', 'settings', { lastBackup: settings.lastBackup });
+            await Server.Database.request('PATCH', 'settings', { lastBackup: settings.lastBackup })
+                .then(async () => {
+                    const id = (await Server.Database.request('GET', 'settings')).testingChannel;
+                    const channel = this.Bot.client.channels.cache.get(id);
+
+                    channel &&
+                        channel.send(`${this.Bot.emojis.get('bot2Success')} <@${dev.id}> **foi realizado o backup automático!**`);
+
+                })
+                .catch(console.log);
             
         }, waiting);
 
