5b54316367872ea7a46d98f4bb52fbc8d35a7585
Kevin <kevinntavares@gmail.com>
2020-10-09 09:53:40 -0300

    Changed .top a bit

--- src/Aiha/Commands/Utility/top.js
+++ src/Aiha/Commands/Utility/top.js
@@ -17,7 +17,9 @@ class Top extends Internals.Command {
 
     async run(msg) {
 
+        const instance = msg.instance;
         const users = await Server.Database.request('GET', 'users');
+        const limit = 10;
 
         if (!Object.keys(users).length) {
             return msg.channel.send(
@@ -35,16 +37,31 @@ class Top extends Internals.Command {
             });
 
         const medals = ['🥇', '🥈', '🥉'];
+        const userData = ranking.find(obj => obj.id === msg.author.id);
 
         const membersRanking = await Promise.all(
-            ranking.map(async (obj, idx) => {
-                const member = await msg.guild.members.fetch(obj.id);
+            ranking.map((obj, idx) => {
+                const member = msg.guild.members.cache.get(obj.id);
 
-                return `**#${++idx}:** \`${member.user.tag}\` LVL ${obj.level} ${medals[idx - 1] || ''}`;
+                return `${
+
+                    !(idx % limit) 
+                        ? `${instance.emojis.get('bot2QuestionMark')}` + (
+                            userData 
+                                ? ` Seu rank atual é: **#${userData.order}**` 
+                                : ' Você não possui um rank.'
+                        ) + '\n\n' 
+                        : ''
+
+                }**#${++idx}:**${
+                    (' ' + Internals.Constants.ZeroWidthSpace).repeat(8)
+                }\`${member ? member.user.tag : obj.id}\`${
+                    (' ' + Internals.Constants.ZeroWidthSpace).repeat(8)
+                }LVL ${obj.level} ${medals[idx - 1] || ''}`;
             })
         );
 
-        new Internals.PageEmbed(msg, membersRanking, 10)
+        new Internals.PageEmbed(msg, membersRanking, limit)
             .setAuthor('Placar do Servidor', msg.guild.iconURL({ dynamic: true }))
             .send();
         
